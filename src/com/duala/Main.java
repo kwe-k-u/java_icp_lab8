23935218e1419a8ffac444031cd6c863e03a6cbf
Kweku <kwekuappiah11@gmail.com>
2021-03-22 18:27:55 +0000

    solved the cpp thing

--- src/com/duala/Main.java
+++ src/com/duala/Main.java
@@ -1,13 +1,19 @@
 package com.duala;
 
 public class Main {
+    static int[][] array ;
+    static int[][] newArray ;
+    static int[] oneDarray;
 
     public static void main(String[] args) {
 	// write your code here
 //        System.out.println(linearIndx(5,2));
 
-        genArray(32);
-//        inverseIndx(17);
+        genArray(128);
+        gen1dArray(128);
+        make2d(128);
+//        display1dArray(8);
+        displayArray(128);
     }
 
 
@@ -29,33 +35,63 @@ public class Main {
     public static int[] inverseIndx(int I){
         int row = 0;
         int column = I;
-        while (row <column){
+        while (row <=column){
             column -= row;
             row++;
         }
         row--;
-        System.out.println(row + ","+ column);
         return new int[]{row, column};
 
     }
 
 
-    public static void genArray(int n){
-        int[][] array = new int[n][n];
+    public static void genArray(int n) {
+        array = new int[n][n];
 
-        for (int row = 0; row < n; row++){
-            for (int column = 0; column <= row; column++){
-                array[row][column] = 1;
+        for (int row = 0; row < n; row++) {
+            for (int column = 0; column <= row; column++) {
+                array[row][column] = 1;//random number
             }
         }
+    }
 
-        System.out.println("array");
+    public static void gen1dArray(int size){
+        int numElements = ((1+size)*size)/2;
+
+        oneDarray = new int[numElements];
+
+        for (int index = 0; index < numElements; index++) {
+            int[] val = inverseIndx(index);
+            oneDarray[index] = array[val[0]][val[1]];
+        }
+
+    }
+
+
+    static void make2d(int size){
+        newArray = new int[size][size];
+        for (int i = 0; i < oneDarray.length; i ++){
+            int[] vals = inverseIndx(i);
+            newArray[vals[0]][vals[1]] = oneDarray[i];
+        }
+    }
+
+
+    static void displayArray(int n){
+
         for (int row = 0; row < n; row++) {
             System.out.print("\nrow"+ row+ " ");
             for (int column = 0; column < n; column++) {
-                System.out.print(array[row][column]);
+                System.out.print(newArray[row][column]);
             }
         }
+    }
+
+
+    static void display1dArray(int size){
+        int numElements = ((1+size)*size)/2;
+        for (int i = 0; i <numElements; i++)
+            System.out.print(oneDarray[i]);
     }
 
 
